5ba5a6b734d41e748edce581a2eea4d7059c1cc3
aliraza13793 <aliraza13793@gmail.com>
2024-02-29 03:59:41 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,39 @@
+#! usr/bin/env node
+import inquirer from "inquirer";
+let conversion = {
+    "PKR": {
+        "PKR": 1,
+        "USD": 0.0036,
+    },
+    "USD": {
+        "USD": 1,
+        "PKR": 279.5313,
+    }
+};
+const answer = await inquirer.prompt([
+    {
+        type: "list",
+        name: "from",
+        choices: ["USD", "PKR"],
+        message: "Select your Currency",
+    },
+    {
+        type: "list",
+        name: 'To',
+        choices: ["USD", "PKR"],
+        message: "Select your conversion currency"
+    },
+    {
+        type: 'number',
+        name: "amount",
+        message: 'Enter your amount for convert'
+    }
+]);
+const { from, To, amount } = answer;
+if (from && To && amount) {
+    let result = conversion[from][To] * amount;
+    console.log(`your converstion from ${from} to ${To} is ${result}`);
+}
+else {
+    console.log("invalid");
+}
